76fa8520b1d09c252d8e0ff82ccf28482a2e62b7
Nsir Dorra <dorransir24@gmail.com>
2023-12-05 11:51:44 +0100

    fixing desgin and setup data base schema

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 module.exports = {
   content: [],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'montserrat': ['Montserrat', 'sans-serif'],
+      },
+      colors: {
+        teal: {
+          '500': '#147E97','550': '#bfa888',
+        },
+        
+      },
+    },
   },
   plugins: [],
 }
