ea4c31b43a8c8af2fab59c478950f73b3f2c3b0d
Khalid Rouissi <107607248+KhalidRouissi1@users.noreply.github.com>
2023-12-24 12:25:43 +0100

    Update build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -1,5 +1,5 @@
 echo "BUILD START"
-python3 --version  # Add this line to check the Python version
-python3 -m pip install -r requirements.txt
-python3 manage.py collectstatic --noinput --clear
+python3.9 --version  # Check the Python version
+python3.9 -m pip install -r requirements.txt
+python3.9 manage.py collectstatic --noinput --clear
 echo "BUILD END"
